53450732c7d78b898cf5407e9612cc27cabffa65
Tipimenta <tiagopimenta.ata@gmail.com>
2022-10-21 12:16:56 -0300

    Feito atualização, colocando mascara nos campo numero, expiração e cvc

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import Imask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -17,3 +18,57 @@ ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
 globalThis.setCardType = setCardType
+
+const securityCode = document.querySelector('#security-code')
+const securityCodePattern = {
+   mask: "0000",
+} 
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+const expirationDate = document.querySelector('#expiration-date')
+const expirationDatePattern = {
+   mask: "MM{/}YY",
+   blocks: {
+      MM: {
+         mask: Imask.MaskedRange,
+         from: 1,
+         to: 12,
+      },
+
+      YY: {
+         mask: Imask.MaskedRange,
+         from: String(new Date().getFullYear()).slice(2),
+         to: String(new Date().getFullYear() + 10).slice(2),
+      },
+   },
+} 
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const CardNumber = document.querySelector('#card-number')
+const CardNumberPattern = {
+   mask: [
+      {
+         mask: "0000 0000 0000 0000",
+         regex: /^4\d{0,15}/,
+         cardtype: "visa",
+      },
+      {
+         mask: "0000 0000 0000 0000",
+         regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+         cardtype: "mastercard",
+      },
+      {
+         mask: "0000 0000 0000 0000",
+         cardtype: "default",
+      },
+   ],
+   dispatch: function(appended, dynamicMasked) {
+      const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+      const foundMask = dunamicMasked.compiledMasks.find(function(item) {
+         return number.match(item.regex)
+      })
+      return foundMask
+   },
+}
+
+const CardNumberMasked = IMask(CardNumber, CardNumberPattern)
